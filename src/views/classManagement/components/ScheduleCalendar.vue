687f128bf82c08ea919ecb057c0a3ab996e91549
邵龙康 <969943919@qq.com>
2025-10-15 09:49:27 +0800

    fix: update activity detail emission in ScheduleCalendar to use classScheduleId instead of item.id

--- src/views/classManagement/components/ScheduleCalendar.vue
+++ src/views/classManagement/components/ScheduleCalendar.vue
@@ -111,7 +111,7 @@
                 :key="activityItem.activityId"
                 :activity="activityItem"
                 :date="item.commonDate"
-                @click="$emit('activity-detail', activityItem.activityId ,item.id ,activityItem.sportType)"
+                @click="$emit('activity-detail', activityItem.activityId ,activityItem.classScheduleId ,activityItem.sportType)"
                 @unbind="$emit('unbind', $event)"
                 @delete="$emit('delete-activity', $event)"
               />
