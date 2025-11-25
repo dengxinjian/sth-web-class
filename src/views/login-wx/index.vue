29e19db4f89b2d01a980adeae00aeb23a6743ddf
zhangyinjun <3023460461@qq.com>
2025-11-25 18:50:20 +0800

    fix：change tips of login error

--- src/views/login-wx/index.vue
+++ src/views/login-wx/index.vue
@@ -217,7 +217,7 @@ export default {
               setToken(res.result.jwt);
               this.$router.push("/timeTable/class");
             } else {
-              this.$message.error("请扫码进入小程序完成运动阈值设置");
+              this.$message.error("请完成引导页设置");
             }
           })
           .catch((error) => {
